ba7945c0b987c890b2f63283f60a869708b5cf94
DmitryStrelkov16 <optimizm16@list.ru>
2020-04-23 23:36:17 +0300

    Add project files.

---
+++ HomeWork1.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HomeWork1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter to string");
+            string str = Console.ReadLine();
+            string str1 = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (i % 2 == 1)
+                str1 = string.Concat(str1, str[i]);
+
+            }
+            Console.WriteLine("New String:");
+            Console.WriteLine(str1);
+           
+        }
+    }
+}
